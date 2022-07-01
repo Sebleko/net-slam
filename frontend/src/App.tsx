581057e932212cec1f9cca3b396215826d4e1bd4
Sebastian Walter <janisebastian100@gmail.com>
2022-07-01 20:17:12 +0300

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import PhotoUploader from './components/PhotoUploader'
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <nav>
+
+      </nav>
+      <div>
+        <PhotoUploader />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/PhotoUploader.tsx
@@ -0,0 +1,33 @@
+
+import { useState } from 'react'
+import './PhotoUploader.css'
+
+function PhotoUploader(){
+    const [imageURL, setImageURL] = useState<string>("")
+
+
+    const handleImageChange = (e: React.FormEvent<HTMLInputElement>) => {
+        const files = e.currentTarget.files;
+        if (files){
+            const img = files[0];
+            
+            setImageURL(() => URL.createObjectURL(files[0]));
+        }
+    }
+
+    return (
+        <section>
+            <form>
+                <label>Select image:</label>
+                <input type="file" id="img" name="img" accept="image/*" onChange={handleImageChange}></input>
+            </form>
+            <div className="image-preview">
+                <div className="image-shell">
+                    <img id="image" src={imageURL} className="image" width="200" />	
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default PhotoUploader
