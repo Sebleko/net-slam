b03ef9369a91b275a73102c2819ebf8ca75ed44d
Sebastian Walter <janisebastian100@gmail.com>
2022-07-12 16:35:18 +0300

    Serve static assets

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,20 @@
 import express, { Express, Request, Response } from "express";
+import path from 'path'
 import dotenv from 'dotenv'
-
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+// Env Vars:
+const port = process.env.PORT || 5000;
+const relative_assets_path = process.env.STATIC_ASSET_FOLDER_PATH || "../frontend/build"
+
+const static_assets_path = path.join(__dirname, "..", relative_assets_path)
+
+app.use(express.static(static_assets_path))
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(static_assets_path, "index.html"))
 });
 
 app.listen(port, () => {
